9030599dae050789b3114bfe32daca5e8f2ee72f
StiffGitt <131384247+StiffGitt@users.noreply.github.com>
2024-05-04 11:34:15 +0200

    łatwiej będzie to rozszerzać

--- sudoku/solver/sudoku_solver_heuristics.h
+++ sudoku/solver/sudoku_solver_heuristics.h
@@ -2,10 +2,12 @@
 #define SUDOKU_SOLVER_HEURISTICS_H
 
 #include "../sudoku-grid/sudoku_grid.h"
+#include "sudoku_heuristics.h"
+
 #define USE_MRV 1
 
 int Sudoku_Grid_solve_heuristics_single_answer(struct Sudoku_Grid* grid, int opt);
-int Sudoku_Grid_solve_heuristics_single_answer_aux(struct Sudoku_Grid* grid, int row, int col, int use_MRV, int use_NP);
+int Sudoku_Grid_solve_heuristics_single_answer_aux(struct Sudoku_Grid* grid, int row, int col, Heuristic_Opts opts);
 int find_next_idx(struct Sudoku_Grid* grid, int row, int col, int use_MRV);
 
 #endif
